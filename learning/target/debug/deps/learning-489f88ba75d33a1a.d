28929f0c99d677b5d44249c0a643a9ae27ad1863
Apollo-XIV <Apollo-XIV@users.noreply.github.com>
2022-12-26 16:36:41 +0000

    initialisation

---
+++ learning/target/debug/deps/learning-489f88ba75d33a1a.d
@@ -0,0 +1,5 @@
+/home/awren/Documents/Rust/Rust-Sketchbook/learning/target/debug/deps/learning-489f88ba75d33a1a.rmeta: src/main.rs
+
+/home/awren/Documents/Rust/Rust-Sketchbook/learning/target/debug/deps/learning-489f88ba75d33a1a.d: src/main.rs
+
+src/main.rs:
---
+++ learning/target/debug/deps/learning-64ea1501c655f996.d
@@ -0,0 +1,5 @@
+/home/awren/Documents/Rust/Rust-Sketchbook/learning/target/debug/deps/learning-64ea1501c655f996.rmeta: src/main.rs
+
+/home/awren/Documents/Rust/Rust-Sketchbook/learning/target/debug/deps/learning-64ea1501c655f996.d: src/main.rs
+
+src/main.rs:
